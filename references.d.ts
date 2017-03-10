9ea679943d417c106d5384426e02fa63b23264ba
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-10 09:50:55 +0100

    fix(build-error): Add references file

---
+++ references.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="./node_modules/tns-core-modules/tns-core-modules.es6.d.ts" /> 
+
+declare type FormData = any;
+declare var FormData;
+declare type Blob = any;
+declare var Blob;
