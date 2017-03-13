2ff5c8d9a74f798dff4c106d6da9547b65af9516
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-13 15:05:19 +0100

    feat(error-handling): Adding basic error handling

--- urlhandler.android.ts
+++ urlhandler.android.ts
@@ -4,11 +4,15 @@ import { getCallback, extractAppURL } from './urlhandler.common';
 
 declare var android: any;
 
-application.android.on(application.AndroidApplication.activityResumedEvent, function (args) {
-    console.log('Event: ' + args.eventName + ', Activity: ' + args.activity);
-    console.log(new String(args.activity.getIntent().getAction()).valueOf(), new String(android.content.Intent.ACTION_VIEW).valueOf());
-    if (new String(args.activity.getIntent().getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
-        let data = args.activity.getIntent().getData();
-        getCallback()(extractAppURL(data));
+application.android.on(application.AndroidApplication.activityResumedEvent, (args) => {
+    try {
+        console.log('Event: ' + args.eventName + ', Activity: ' + args.activity);
+        console.log(new String(args.activity.getIntent().getAction()).valueOf(), new String(android.content.Intent.ACTION_VIEW).valueOf());
+        if (new String(args.activity.getIntent().getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
+            let data = args.activity.getIntent().getData();
+            getCallback()(extractAppURL(data));
+        }
+    } catch (e) {
+        console.error('Unknown error during getting App URL data', e);
     }
 });
