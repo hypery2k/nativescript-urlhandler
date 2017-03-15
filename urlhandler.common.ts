5c821a894df271176d6ff98531e7992bf364a73b
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-15 15:14:22 +0100

    feat(extract-url): Finalize URL extraction

--- urlhandler.common.ts
+++ urlhandler.common.ts
@@ -1,20 +1,33 @@
 import { AppURL, UrlHandlerCallback } from './urlhandler';
 var URL_HANDLER_CB: UrlHandlerCallback;
 
+/**
+ * Extract url string param to a nice object
+ * @param urlParam to parse
+ * @returns AppURL object or null if invalid was used
+ */
 export function extractAppURL(urlParam: any): AppURL {
-    let url = <String>urlParam,
-        params = new Map<String, String>(),
-        path = url.substring(url.indexOf('//') + 2, url.indexOf('?')),
-        parameters = url.substring(url.indexOf('?') + 1).split('&');
-    // create JSON object
-    for (let i = 0, len = parameters.length; i < len; i++) {
-        let paramData = params[i].split('=');
-        params.set(paramData[0], paramData[1] ? paramData[1] : null);
+    if (!!urlParam) {
+        let url = urlParam.toString(),
+            params = new Map<String, String>(),
+            urlWithPath = url.indexOf('//') < url.length - 2,
+            urlWithParams = url.indexOf('?') !== -1,
+            path = urlWithPath ? url.substring(url.indexOf('//') + 2, urlWithParams ? url.indexOf('?') : url.length) : null,
+            parameters = url.substring(url.indexOf('?') + 1).split('&');
+        // create JSON object
+        if (urlWithParams) {
+            for (let i = 0, len = parameters.length; i < len; i++) {
+                let paramData = parameters[i].split('=');
+                params.set(paramData[0], paramData[1] ? paramData[1] : null);
+            }
+        }
+        return {
+            params: params,
+            path: path
+        };
+    } else {
+        return null;
     }
-    return {
-        params: params,
-        path: path
-    };
 }
 
 export function handleOpenURL(handler: UrlHandlerCallback): void {
