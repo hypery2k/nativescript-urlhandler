d810a4064b1c8fcd973fab056173a8ced4eddd43
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-20 10:04:23 +0100

    fix(reinvocation-error): Remember state to no invoke a second time
    see #9

--- urlhandler.android.ts
+++ urlhandler.android.ts
@@ -4,15 +4,18 @@ import { getCallback, extractAppURL } from './urlhandler.common';
 export { handleOpenURL } from './urlhandler.common';
 declare var android: any;
 
+let alreadyHandled: boolean = false;
+
 application.android.on(application.AndroidApplication.activityResumedEvent, (args) => {
-    try {
-        console.log('Event: ' + args.eventName + ', Activity: ' + args.activity);
-        console.log(new String(args.activity.getIntent().getAction()).valueOf(), new String(android.content.Intent.ACTION_VIEW).valueOf());
-        if (new String(args.activity.getIntent().getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
-            let data = args.activity.getIntent().getData();
-            getCallback()(extractAppURL(data));
+    if (!alreadyHandled) {
+        try {
+            if (new String(args.activity.getIntent().getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
+                let data = args.activity.getIntent().getData();
+                getCallback()(extractAppURL(data));
+                alreadyHandled = true;
+            }
+        } catch (e) {
+            console.error('Unknown error during getting App URL data', e);
         }
-    } catch (e) {
-        console.error('Unknown error during getting App URL data', e);
     }
 });
