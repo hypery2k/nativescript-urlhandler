bf72ff32d38bcc9132f347e0d35f8099b22a685b
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-24 19:51:51 +0100

    refactor(android): Revert Proxy changes
    use event listener and add create event listener, see #16

--- urlhandler.android.ts
+++ urlhandler.android.ts
@@ -1,6 +1,5 @@
 /// <reference path='node_modules/tns-platform-declarations/android/android.d.ts' />
 import * as application from 'application';
-import { setActivityCallbacks, AndroidActivityCallbacks } from 'ui/frame';
 import { getCallback, extractAppURL } from './urlhandler.common';
 export { handleOpenURL } from './urlhandler.common';
 
@@ -12,8 +11,9 @@ export function handleIntent(intent: any) {
         try {
             if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
                 application.android.on(application.AndroidApplication.activityResultEvent, (eventData) => {
-                    getCallback()(extractAppURL(data));
-                    lastReceivedData = data;
+                    setTimeout(() => {
+                        getCallback()(extractAppURL(data));
+                    });
                 });
             }
         } catch (e) {
@@ -21,23 +21,30 @@ export function handleIntent(intent: any) {
         }
     }
 }
-
-@JavaProxy('com.tns.NativeScriptActivity')
-export class Activity extends android.app.Activity {
-    private _callbacks: AndroidActivityCallbacks;
-
-    protected onCreate(savedInstanceState: android.os.Bundle): void {
-        if (!this._callbacks) {
-            setActivityCallbacks(this);
-        }
-        // initialize the modules with the custom application object
-        application.android.init(this);
-        this._callbacks.onCreate(this, savedInstanceState, super.onCreate);
-    }
-    protected onNewIntent(intent: android.content.Intent): void {
-        super.onNewIntent(intent);
-        if (intent.getDataString) {
-            handleIntent(intent);
+application.android.on(application.AndroidApplication.activityCreatedEvent, (args) => {
+    let intent: android.content.Intent = args.activity.getIntent(),
+        data = intent.getData();
+    if (data !== lastReceivedData) {
+        try {
+            if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
+                handleIntent(intent);
+            }
+        } catch (e) {
+            console.error('Unknown error during getting App URL data', e);
         }
     }
-}
+});
+application.android.on(application.AndroidApplication.activityResumedEvent, (args) => {
+    let intent: android.content.Intent = args.activity.getIntent(),
+        data = intent.getData();
+    if (data !== lastReceivedData) {
+        try {
+            if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
+                handleIntent(intent);
+                lastReceivedData = data;
+            }
+        } catch (e) {
+            console.error('Unknown error during getting App URL data', e);
+        }
+    }
+});
