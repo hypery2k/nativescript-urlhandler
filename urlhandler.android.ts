76ceef4c0365370df521afca519f435b9ab0bd88
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-04-06 16:41:19 +0200

    fix(action-error): Resolve action error on android

--- urlhandler.android.ts
+++ urlhandler.android.ts
@@ -9,31 +9,26 @@ export function handleIntent(intent: any) {
     let data = intent.getData();
     if (lastReceivedData === null || data.toString() !== lastReceivedData.toString()) {
         try {
-            if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
+            if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_MAIN).valueOf()
+                || new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
                 try {
-                    setTimeout(() => {
-                        getCallback()(extractAppURL(data));
-                    });
+                    setTimeout(() => getCallback()(extractAppURL(data)));
                 } catch (ignored) {
                     application.android.on(application.AndroidApplication.activityResultEvent, (eventData) => {
-                        setTimeout(() => {
-                            getCallback()(extractAppURL(data));
-                        });
+                        setTimeout(() => getCallback()(extractAppURL(data)));
                     });
                 }
             }
         } catch (e) {
             console.error('Unknown error during getting App URL data', e);
         }
+        lastReceivedData = data;
     }
 }
 application.android.on(application.AndroidApplication.activityCreatedEvent, (args) => {
     let intent: android.content.Intent = args.activity.getIntent();
     try {
-        if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_MAIN).valueOf()
-            || new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
-            handleIntent(intent);
-        }
+        handleIntent(intent);
     } catch (e) {
         console.error('Unknown error during getting App URL data', e);
     }
@@ -42,11 +37,7 @@ application.android.on(application.AndroidApplication.activityCreatedEvent, (arg
 application.android.on(application.AndroidApplication.activityResumedEvent, (args) => {
     let intent: android.content.Intent = args.activity.getIntent();
     try {
-        if (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_MAIN).valueOf()
-            || new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf()) {
-            handleIntent(intent);
-            lastReceivedData = intent.getData();
-        }
+        handleIntent(intent);
     } catch (e) {
         console.error('Unknown error during getting App URL data', e);
     }
