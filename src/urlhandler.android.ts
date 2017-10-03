808617a7e07c736aa13c15b358447abd325eec96
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-10-03 06:21:38 +0200

    drop(received-data): Ignore last received data from now on
    see #69

--- src/urlhandler.android.ts
+++ src/urlhandler.android.ts
@@ -3,29 +3,25 @@ import * as application from 'application';
 import { getCallback, extractAppURL } from './urlhandler.common';
 export { handleOpenURL } from './urlhandler.common';
 
-let lastReceivedData = null;
-
 export function handleIntent(intent: any) {
     let data = intent.getData();
-    if (lastReceivedData === null || data.toString() !== lastReceivedData.toString()) {
-        try {
-            let appURL = extractAppURL(data);
-            if (appURL != null &&
-                (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_MAIN).valueOf()
-                    || new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf())) {
-                try {
+    try {
+        let appURL = extractAppURL(data);
+        if (appURL != null &&
+            (new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_MAIN).valueOf()
+                || new String(intent.getAction()).valueOf() === new String(android.content.Intent.ACTION_VIEW).valueOf())) {
+            try {
+                setTimeout(() => getCallback()(appURL));
+            } catch (ignored) {
+                application.android.on(application.AndroidApplication.activityResultEvent, (eventData) => {
                     setTimeout(() => getCallback()(appURL));
-                } catch (ignored) {
-                    application.android.on(application.AndroidApplication.activityResultEvent, (eventData) => {
-                        setTimeout(() => getCallback()(appURL));
-                    });
-                }
+                });
             }
-        } catch (e) {
-            console.error('Unknown error during getting App URL data', e);
         }
-        lastReceivedData = data;
+    } catch (e) {
+        console.error('Unknown error during getting App URL data', e);
     }
+
 }
 application.android.on(application.AndroidApplication.activityCreatedEvent, (args) => {
     let intent: android.content.Intent = args.activity.getIntent();
